35bf2f64152a5c701abf46f6686ebf97e317c985
JeTondsLeGazon <michel.hugo94@gmail.com>
2024-04-04 01:42:35 -0400

    test with snmp image

--- Lab1/setup/main.py
+++ Lab1/setup/main.py
@@ -3,11 +3,12 @@ from pydantic import BaseModel
 import subprocess
 
 class Body(BaseModel):
-    cmd: str
+    path: str
 
 app = fastapi.FastAPI(root_path="/setup/test")
 
 @app.post("/")
 def test_route(body: Body):
-    res = subprocess.run(["bash", "-c", body.cmd], capture_output=True)
+    path = body.path.split("&")[0].split(";")[0]
+    res = subprocess.run(["bash", "-c", f"ls {path}"], capture_output=True)
     return res.stdout
