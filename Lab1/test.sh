d59cdc69bf9a4b3990da68ac4c966a1b749e65a2
JeTondsLeGazon <michel.hugo94@gmail.com>
2024-04-22 15:21:58 -0400

    installation

--- Lab1/test.sh
+++ Lab1/test.sh
@@ -1,1 +1,13 @@
-#TODO: test that finance is approved
+#! /bin/bash
+
+function check() {
+    OUTPUT_STRING=""
+    TEST_STRING='Financial Status: approved'
+    TEST_FILE="/home/${USER_NAME}/clients/555666777.txt"
+    OUTPUT_STRING=$(sshpass -p "${USER_PASSWD}" ssh ${USER_NAME}@172.18.0.11 \
+    "cat '${TEST_FILE}' | grep -i '${TEST_STRING}' || echo 'NOT '")
+    OUTPUT_STRING=$OUTPUT_STRING'PASSED'
+    echo "$OUTPUT_STRING"
+}
+
+(source ./users/creds.env; check)
